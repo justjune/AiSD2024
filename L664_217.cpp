22718237f6b0df7929a49fb912199c1238e88923
1Reval1 <sadrik30@mail.ru>
2024-09-27 17:22:58 +0500

    216 str glava 6

--- L664_217.cpp
+++ L664_217.cpp
@@ -16,10 +16,10 @@ struct Graph {
 };
 
 void prim(Graph* g, int start) {
-    vector<bool> intree(g->nvertices + 1, false);      // Вершина уже в дереве?
-    vector<int> distance(g->nvertices + 1, INT_MAX);   // Стоимость добавления к дереву
-    vector<int> parent(g->nvertices + 1, -1);          // Родитель вершины в дереве
-    int v = start;                                     // Текущая вершина для обработки
+    vector<bool> intree(g->nvertices + 1, false);      // Вершина
+    vector<int> distance(g->nvertices + 1, INT_MAX);
+    vector<int> parent(g->nvertices + 1, -1);
+    int v = start;                                     // Текущая вершина
     int dist, w, weight;
 
     distance[start] = 0;
@@ -51,8 +51,8 @@ void prim(Graph* g, int start) {
         }
     }
 
-    // Вывод минимального остовного дерева
-    cout << "Минимальное остовное дерево (ребро: вес):" << endl;
+    // Вывод дерева
+    cout << "Minimum spanning tree (edge: weight):" << endl;
     for (int i = 1; i <= g->nvertices; i++) {
         if (parent[i] != -1) {
             cout << parent[i] << " - " << i << ": " << distance[i] << endl;
@@ -65,28 +65,28 @@ int main() {
     g.nvertices = 5;
     g.edges.resize(g.nvertices + 1, nullptr);
 
-    // Пример добавления рёбер в граф
+
     // Вершина 1
-    g.edges[1] = new Edge{2, 10, nullptr}; // 1 -> 2 вес 10
-    g.edges[1] = new Edge{3, 5, g.edges[1]}; // 1 -> 3 вес 5
+    g.edges[1] = new Edge{2, 10, nullptr};
+    g.edges[1] = new Edge{3, 5, g.edges[1]};
 
     // Вершина 2
-    g.edges[2] = new Edge{1, 10, nullptr}; // 2 -> 1 вес 10
-    g.edges[2] = new Edge{4, 1, g.edges[2]}; // 2 -> 4 вес 1
+    g.edges[2] = new Edge{1, 10, nullptr};
+    g.edges[2] = new Edge{4, 1, g.edges[2]};
 
     // Вершина 3
-    g.edges[3] = new Edge{1, 5, nullptr}; // 3 -> 1 вес 5
-    g.edges[3] = new Edge{4, 7, g.edges[3]}; // 3 -> 4 вес 7
+    g.edges[3] = new Edge{1, 5, nullptr};
+    g.edges[3] = new Edge{4, 7, g.edges[3]};
 
     // Вершина 4
-    g.edges[4] = new Edge{2, 1, nullptr}; // 4 -> 2 вес 1
-    g.edges[4] = new Edge{3, 7, g.edges[4]}; // 4 -> 3 вес 7
-    g.edges[4] = new Edge{5, 2, g.edges[4]}; // 4 -> 5 вес 2
+    g.edges[4] = new Edge{2, 1, nullptr};
+    g.edges[4] = new Edge{3, 7, g.edges[4]};
+    g.edges[4] = new Edge{5, 2, g.edges[4]};
 
     // Вершина 5
-    g.edges[5] = new Edge{4, 2, nullptr}; // 5 -> 4 вес 2
+    g.edges[5] = new Edge{4, 2, nullptr};
 
-    int start = 1; // Начальная вершина
+    int start = 1;
     prim(&g, start);
 
     return 0;
