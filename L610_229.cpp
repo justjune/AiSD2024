90e5f5290d119689c6b1329558162b83292446ff
delogin <fedorrsteam@gmail.com>
2024-10-04 00:14:55 +0500

    asd

--- L610_229.cpp
+++ L610_229.cpp
@@ -1,27 +1,26 @@
 #include "graph.h"
 #include <iostream>
-int parent[MAXV+1];
 void dijkstra(graph *g, int start){
-    edgenode *p;
     bool intree[MAXV+1];
     int distance[MAXV+1];
-    int v;
+    int parent[MAXV+1];
+    int v = start;
     int w;
     int weight;
     int dist;
+    distance[start] = 0;
     for (int i = 1; i <= g->nvertices; i++){
         intree[i] = false;
         distance[i] = MAXINT;
         parent[i] = -1;
     }
-    distance[start] = 0;
-    v = start;
-    while (intree[v] == false)
+    while (!intree[v])
     {
         intree[v] = true;
-        p = g->edges[v];
+        edgenode* p = g->edges[v];
         while (p!=nullptr)
         {
+            std::cout << p->weight << std::endl;
             w = p->y;
             weight = p->weight;
             if (distance[w] > (distance[v]+weight))
@@ -47,4 +46,12 @@ void dijkstra(graph *g, int start){
         }
     }
     
+}
+int main() {
+    graph g;
+    read_graph(&g, false);
+    int start = 3;
+    dijkstra(&g, start);
+
+    return 0;
 }
