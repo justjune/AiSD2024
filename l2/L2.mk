f771205804a4414c72acbf19e5b5b123cece2bde
Reval Sadrislamov <Reval.Sadrislamov@urfu.me>
2025-01-13 21:16:08 +0500

    l2

--- l2/L2.mk
+++ l2/L2.mk
@@ -15,3 +15,21 @@ $(funk).o : $(funk).cpp
 
 clean:
 	$(rm) *.o $(spp).exe
+
+
+
+#L224_063.
+
+spp2 = L224_063
+mat2 = matx
+$(spp2).exe : $(spp2).o $(mat2).o
+	$(CXX) $(spp2).o $(mat2).o -o $(spp2)
+
+$(spp2).o : $(spp2).cpp
+	$(CXX) -c $(spp2).cpp -o $(spp2).o
+
+$(mat2).o : $(mat2).cpp
+	$(CXX) -c $(mat2).cpp -o $(mat2).o
+
+clean:
+	$(rm) *.o $(spp2).exe
---
+++ l2/matx.h
@@ -0,0 +1,7 @@
+#ifndef MATX_H
+#define MATX_H
+
+// Указываем размеры матрицы как параметры функции
+void processMatrix(int rows, int cols, int matrix[][5]);
+
+#endif // MATX_H
