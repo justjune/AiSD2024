6a26eec3f4ca466d69f9ec3af6c0be190421251d
Ilya Tkachenko <tkachenko.ilya.00@mail.ru>
2025-01-14 16:00:09 +0300

    Listing22

---
+++ L2/L22.mk
@@ -0,0 +1,20 @@
+CXX = g++
+RM = del
+LISTING = L222_060
+FUNCTION_NAME = myfunc
+
+# Сборка исполняемого файла
+$(LISTING).exe : $(LISTING).o $(FUNCTION_NAME).o
+	$(CXX) $(LISTING).o $(FUNCTION_NAME).o -o $(LISTING).exe
+
+# Компиляция объектного файла программы
+$(LISTING).o : $(LISTING).cpp
+	$(CXX) -c $(LISTING).cpp -o $(LISTING).o
+
+# Компиляция объектного файла функции
+$(FUNCTION_NAME).o : $(FUNCTION_NAME).cpp $(FUNCTION_NAME).h
+	$(CXX) -c $(FUNCTION_NAME).cpp -o $(FUNCTION_NAME).o
+
+# Очистка
+clean:
+	$(RM) *.o $(LISTING).exe
