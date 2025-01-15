15c1785ffb51f7140fe568b719013ef20aa7f07a
Nikita <velichko_nikitka@mail.ru>
2025-01-15 19:47:42 +0500

    L8 hpp rewrite's

--- 8 chapter/L809_305.h
+++ 8 chapter/L809_305.h
@@ -9,24 +9,6 @@ match_out(char s, char t, int i, int j);
 insert_out(char t, int j);
 delete_out(char s, int i);
 
-reconstruct_path(char *s, char *t, int i, int j)
-{
-	if (m[i][j].parent == -1) return;
-	if (m[i][j].parent == MATCH) {
-		reconstruct_path(s, t, i-1, j-1);
-		match_out(s, t, i, j);
-		return;
-	}
-	if (m[i][j].parent == MATCH) {
-		reconstruct_path(s, t, i, j-1);
-		insert_out(t, j);
-		return;
-	}
-	if (m[i][j].parent == DELETE) {
-		reconstruct_path(s, t, i-1, j);
-		delete_out(s, i);
-		return;
-	}
-}
+reconstruct_path(char *s, char *t, int i, int j);
 
 #endif //L809_305_h
