2a2f2108d6cf30b9571ed49aac0e96ef49243559
delogin <fedorrsteam@gmail.com>
2025-09-17 16:18:34 +0500

    test

---
+++ L402_147/L402_147test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "L402_147.hpp"
+
+using namespace std;
+
+int main(){
+    cout << "Test: start testing L402_147\n";
+    cout << "pq_parent for 2 is " << pq_parent(2) << endl;
+    cout << "pq_young_child for 5 is " << pq_young_child(5) << endl;
+    cout << "Test: test L402_147 finished\n";
+}
